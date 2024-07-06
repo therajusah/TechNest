2561b6ae27c96faff442dbc046b07c1b472712fa
therajusah <workwithrajukumar@gmail.com>
2024-07-06 08:24:45 +0530

    Updated Home component: rearranged CSS classes, updated Navbar position, and replaced Lorem Ipsum text with new content.

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -2,19 +2,22 @@ import Navbar from "./Navbar";
 
 const Home = () => {
   return (
-    <div className="h-screen w-full bg-gradient-to-r from-purple-500 to-indigo-600">
+    <div className="w-full h-screen bg-gradient-to-r from-purple-500 to-indigo-600">
       <Navbar />
-      <div className="container mx-auto px-4 py-16 text-center">
+      <div className="container px-4 py-16 mx-auto text-center">
         <div className="max-w-3xl mx-auto text-center">
-          <h1 className="text-5xl font-bold text-white mb-8">
-            Lorem ipsum dolor sit amet consectetur.
+          <h1 className="mb-8 text-5xl font-bold text-white">
+            Hey, Welcome to TechNest Technical CLub
           </h1>
           <p className="text-lg text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
-            voluptate repellat recusandae voluptatibus dolorum ab deserunt.
-            Libero quidem perspiciatis autem asperiores ut impedit doloremque
-            repellendus! Adipisci nemo earum fugiat accusantium.
-          </p>
+        🚀 Join us for an electrifying TechFest at Supaul College of Engineering! <br />🌟 Calling all tech enthusiasts! Prepare to ignite your passion for innovation and collaboration at our upcoming TechFest. 
+        <br />
+        📅 Save the Date: 
+        <br />
+        📍 Location: Supaul College of Engineering, Supaul 
+        <br />
+        Whether you&#39;re a seasoned coder or just diving into the world of technology, this event is your platform to shine. Let&#39;s hack, create, and shape the future together! Stay tuned for event specifics, registration details, and exciting updates. Don&#39;t miss this chance to explore cutting-edge tech, network with like-minded individuals, and make lasting memories.
+      </p>
         </div>
       </div>
     </div>
